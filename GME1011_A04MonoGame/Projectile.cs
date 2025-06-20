bc8b595e01719ee63af8a37014d3a20ccc584c07
Billboard-SP <A00312828@mycambrian.ca>
2025-06-20 01:45:14 -0400

    projectile code

---
+++ GME1011_A04MonoGame/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace GME1011_A04MonoGame
+{
+    public class Projectile
+    {
+        public Texture2D Texture;
+        public Vector2 Position;
+        public Vector2 Direction;
+        public float Speed = 400f;
+        public bool IsFromPlayer;
+        public int Damage = 1;
+        public Rectangle Bounds => new((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+
+        public Projectile(Texture2D texture, Vector2 position, Vector2 direction, bool isFromPlayer)
+        {
+            Texture = texture;
+            Position = position;
+            Direction = direction;
+            IsFromPlayer = isFromPlayer;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Position += Direction * Speed * elapsed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
