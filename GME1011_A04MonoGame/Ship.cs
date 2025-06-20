94f510deed3067dffc93ec30c7b15af5f41f4a2c
Billboard-SP <A00312828@mycambrian.ca>
2025-06-20 00:46:53 -0400

    Ship.cs

---
+++ GME1011_A04MonoGame/Ship.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace GME1011_A04MonoGame
+{
+    public abstract class Ship
+    {
+        public Texture2D Texture;
+        public Vector2 Position;
+        public int Health;
+        public Rectangle Bounds => new((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+
+        public Ship(Texture2D texture, Vector2 position, int health)
+        {
+            Texture = texture;
+            Position = position;
+            Health = health;
+        }
+
+        public abstract void Update(GameTime gameTime, List<Projectile> projectiles, Texture2D projectileTexture);
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
